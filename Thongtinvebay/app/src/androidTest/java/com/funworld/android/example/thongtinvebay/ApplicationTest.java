ff9bea9c1912e5a2bcfb333a73b30c75a3589c8b
Tran Duy Thanh <thanhtd1987@gmail.com>
2016-03-03 16:00:14 +0700

    [GENERAL] add project

---
+++ Thongtinvebay/app/src/androidTest/java/com/funworld/android/example/thongtinvebay/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.funworld.android.example.thongtinvebay;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
