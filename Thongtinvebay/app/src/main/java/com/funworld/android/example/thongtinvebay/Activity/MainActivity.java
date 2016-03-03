53af7bc509737d7a1c71d112d1ef6f39ca432f83
Tran Duy Thanh <thanhtd1987@gmail.com>
2016-03-03 17:09:26 +0700

    [UI] add main screen

---
+++ Thongtinvebay/app/src/main/java/com/funworld/android/example/thongtinvebay/Activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.funworld.android.example.thongtinvebay.Activity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Windows on 3/3/2016.
+ */
+public class MainActivity extends AppCompatActivity{
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+    }
+}
