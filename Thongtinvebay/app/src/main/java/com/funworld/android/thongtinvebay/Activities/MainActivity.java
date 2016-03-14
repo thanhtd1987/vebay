30b2c755261c94344dd6b2b3183c4d47c178378c
Tran Duy Thanh <thanhtd1987@gmail.com>
2016-03-14 17:03:11 +0700

    [UI] add new Main UI

--- Thongtinvebay/app/src/main/java/com/funworld/android/thongtinvebay/Activities/MainActivity.java
+++ Thongtinvebay/app/src/main/java/com/funworld/android/thongtinvebay/Activities/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_activity);
+        setContentView(R.layout.main_ui_2);
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
 //
